de8ee37cfc3b350f5bfc90204ef43e817fb6a29f
ChirilovDoralina <57265365+ChirilovDoralina@users.noreply.github.com>
2019-11-01 18:37:16 +0200

    Create T1_E1_Chirilov_Doralina_2ms.m

---
+++ T1_E1_Chirilov_Doralina_2ms.m
@@ -0,0 +1,13 @@
+t=0:.002:8;
+%4 perioade/2ms
+x=0.75*square(pi*t,25)-0.25;
+%->Atribuim variabilei x functia unui semnal dreptunghiular
+%->Folosim functia plot pentru a genera un grafic cu parametrii t si x
+plot(t,x)
+%->Folosim functia axis pentru a delimita spectrele de valori alegraficului generat de plot pentru a se incadra corespunzator in parametri
+axis([0 8 -2 2])
+%->Am denumit axa Ox
+xlabel('Timpul(secunde)')
+%->Am denumit axa Oy
+ylabel('Amplitudinea semnalului(um)')
+title('Semnal dreptunghiular cu factor de umplere 25%') 
